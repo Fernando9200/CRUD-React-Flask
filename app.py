27b835c0bb6996759b539d98a354cf4263c695c1
Fernando Rafael Carretto Sosa <fernando91.sosa@hotmail.com>
2023-11-10 22:59:55 -0300

    Project started

---
+++ app.py
@@ -0,0 +1,14 @@
+from flask import Flask, jsonify, request
+
+app = Flask(__name__)
+
+@app.route("/")
+def hello_world():
+    return "<p>Hello, world!</p>"
+
+@app.route('/useradd', methods=['POST'])
+def useradd():
+    name = request.json['name']
+    email = request.json['email']
+
+    return jsonify({"success": "Success post"})
